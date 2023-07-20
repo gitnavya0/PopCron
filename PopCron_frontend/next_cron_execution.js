95a965f28dd8596acfecef6070eb79562a375a24
AdithiKashain <94888785+AdithiKashain@users.noreply.github.com>
2023-07-20 22:31:56 +0530

    Update next_cron_execution.js, converted time to ist

--- PopCron_frontend/next_cron_execution.js
+++ PopCron_frontend/next_cron_execution.js
@@ -1,11 +1,12 @@
-const cronParser = require('cron-parser');
+const cron = require('cron-parser');
 
 function getNextCronExecutionTime(cronExpression) {
-    const interval = cronParser.parseExpression(cronExpression);
-    const nextExecutionTime = interval.next();
-    const getNextCronExecutionTime = nextExecutionTime.toISOString();
-
-    return getNextCronExecutionTime;
+    const interval = cron.parseExpression(cronExpression, { tz: 'Asia/Kolkata' }); // Set timezone to IST
+    const now = new Date(); // Get the current time in UTC
+    const nextExecutionTime = interval.next().toDate();
+    const nextExecutionTimeIST = new Date(nextExecutionTime.getTime() + (5.5 * 60 * 60 * 1000)); // Convert to IST
+    
+    return nextExecutionTimeIST.toISOString();
 }
 
 module.exports = { getNextCronExecutionTime };
