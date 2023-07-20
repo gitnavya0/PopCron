9994cf67955df747d58e6513d5ba5b3a590ad5d0
Navya <93501878+gitnavya0@users.noreply.github.com>
2023-07-20 22:33:52 +0530

    Update next_cron_execution.js

--- PopCron_frontend/next_cron_execution.js
+++ PopCron_frontend/next_cron_execution.js
@@ -1,10 +1,10 @@
 const cron = require('cron-parser');
 
 function getNextCronExecutionTime(cronExpression) {
-    const interval = cron.parseExpression(cronExpression, { tz: 'Asia/Kolkata' }); // Set timezone to IST
-    const now = new Date(); // Get the current time in UTC
+    const interval = cron.parseExpression(cronExpression, { tz: 'Asia/Kolkata' }); 
+    const now = new Date(); 
     const nextExecutionTime = interval.next().toDate();
-    const nextExecutionTimeIST = new Date(nextExecutionTime.getTime() + (5.5 * 60 * 60 * 1000)); // Convert to IST
+    const nextExecutionTimeIST = new Date(nextExecutionTime.getTime() + (5.5 * 60 * 60 * 1000)); 
     
     return nextExecutionTimeIST.toISOString();
 }
