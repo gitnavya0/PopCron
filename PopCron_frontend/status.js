d5774c9580d43ac7032f5228e05a5a57952bdf63
Navya <93501878+gitnavya0@users.noreply.github.com>
2023-07-27 13:11:00 +0530

    Update status.js

--- PopCron_frontend/status.js
+++ PopCron_frontend/status.js
@@ -1,29 +1,31 @@
-function getTaskStatus(taskType, schedule) {
-    if (taskType === 'event') {
-        const currentTime = new Date();
-        const scheduledTime = new Date(schedule);
+//status rn changes only when refreshed. 
+function updateJobStatus(jobs) {
+    const currentTimeUTC = Date.now();
 
-        if (currentTime < scheduledTime) {
-            return 'Yet to be done';
-        } 
-        else if(currentTime > scheduledTime) {
-            return 'Already done';
-        }
-        else {
-            return 'In progress';
-        }
-    } else if (taskType === 'cron') {
-        const currentTime = new Date();
-        const nextScheduledTime = new Date(schedule);
+    for (const job of jobs) {
+        const scheduledTime = new Date(job.schedule).getTime();
+        const currentTimeIST = new Date(currentTimeUTC + (5 * 60 + 30) * 60 * 1000);
 
-        if (currentTime < nextScheduledTime) {
-            return 'In progress';
-        } else {
-            return 'Yet to be done';
+        if (job.taskType === 'event') {
+            if (currentTimeIST < scheduledTime) {
+                job.status = 'scheduled';
+            } else if (currentTimeIST >= scheduledTime && currentTimeIST < scheduledTime + 60000) {
+                job.status = 'in progress'; // doesnt check if the event was actually run but just shows in progress for a minute. 
+            } else {
+                job.status = 'completed';
+            } //refresh after a minute it'll show completed. 
+        } else if (job.taskType === 'cron') {
+            if (currentTimeIST < scheduledTime) {
+                job.status = 'scheduled';
+            } //this shows in progress for a minute doesnt check if cron was actually run.
+            else if (currentTimeIST >= scheduledTime && currentTimeIST < scheduledTime + 60000) {
+                job.status = 'in progress';
+            } else { // once executed, calculate next time of execution and make it scheduled again. 
+                //job.schedule = getNextCronExecutionTime(job.cron_exp).toISOString();
+                job.status = 'calculate next execution time then scheduled.';
+            }
         }
-    } else {
-        return 'Unknown';
     }
 }
 
-module.exports = { getTaskStatus };
+module.exports = { updateJobStatus };
