7b2e3a38325edf1a57a39409b2c546a3622bb45c
Navya <93501878+gitnavya0@users.noreply.github.com>
2023-08-15 10:23:22 +0530

    Update sending_get_request.js

--- sending_get_request.js
+++ sending_get_request.js
@@ -11,9 +11,10 @@ const sendGetRequests = async (executable_jobs) => {
         }
 
         for (const job of executable_jobs) {
-            const { url, _id, title, time } = job;
+            const { url, _id, title } = job;
 
             let responseReceived = false;
+            let Error = '';
 
             try {
                 const response = await new Promise((resolve, reject) => {
@@ -23,12 +24,35 @@ const sendGetRequests = async (executable_jobs) => {
                 if (response.statusCode === 200) {
                     console.log(`Response received for job ${_id} from URL ${url}`);
                     responseReceived = true;
+                    if (url === 'http://www.testingmcafeesites.com/testcat_al.html') {
+                        const sendMailOptions = {
+                            hostname: 'localhost',
+                            port: 3002,
+                            path: `/sendAttendanceReport`,
+                            method: 'GET'
+                        };
+
+                        const sendMailRequest = http.request(sendMailOptions, (response) => {
+                            if (response.statusCode === 200) {
+                                console.log(`GET request to sendMail.js for job ${_id} sent successfully to send attendance report `);
+                            } else {
+                                console.error(`Received a non-successful response for job ${_id} from sendMail.js to send attendance report `);
+                            }
+                        });
+
+                        sendMailRequest.on('error', (error) => {
+                            console.error(`Error while sending GET request to sendMail.js to send attendance report  for job ${_id}:`, error.message);
+                        });
+
+                        sendMailRequest.end();
+                    }
                 } else {
                     console.error(`Received a non-successful response for job ${_id} from URL ${url}`);
                     responseReceived = false;
                 }
             } catch (error) {
                 console.error(`Error while fetching job ${_id} from URL ${url}:`, error.message);
+                Error = error.message;
             } finally {
                 if (job.taskType === 'cron' && responseReceived == false) {
                     job.version += 1;
@@ -68,12 +92,14 @@ const sendGetRequests = async (executable_jobs) => {
                     job.status = responseReceived ? 'successful' : 'failed';
                     await job.save();
                 }
+
                 const encodedTitle = encodeURIComponent(title);
+                const encodedError = encodeURIComponent(Error);
 
                 const sendMailOptions = {
                     hostname: 'localhost',
-                    port: 4000,
-                    path: `/send-email?status=${responseReceived}&title=${encodedTitle}`,
+                    port: 3001,
+                    path: `/send-email?status=${responseReceived}&title=${encodedTitle}&Error=${encodedError}`,
                     method: 'GET'
                 };
 
